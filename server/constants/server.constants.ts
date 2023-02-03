9893ff20ed884fc7b8311903db2366450aab54c0
Gurmukh Singh <gurmukhsingh.int@delhivery.com>
2023-02-03 18:41:51 +0530

    ignoring new  files

--- server/constants/server.constants.ts
+++
@@ -1,4 +0,0 @@
-export const PORT = 4000;
-export const NODE_IP = '172.17.0.2';
-export const DATA_CENTER = 'datacenter1';
-export const KEYSPACE = 'database'
--- server/dist/constants/server.constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KEYSPACE = exports.DATA_CENTER = exports.NODE_IP = exports.PORT = void 0;
-exports.PORT = 4000;
-exports.NODE_IP = '172.17.0.2';
-exports.DATA_CENTER = 'datacenter1';
-exports.KEYSPACE = 'database';
